aa24ca34b48adcb522fe610d8ac1d5b8e7a24701
Konstantinos Zotos <kzwtos@gmail.com>
2019-10-25 13:56:30 +0300

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+	function openSubscribeMenu() {
+		var popupSubscribe = document.getElementById("popup-subscribe-main");
+		popupSubscribe.style.display ="block";
+	}
+
+	function closeSubscribeMenu() {
+		var popupSubscribe = document.getElementById("popup-subscribe-main");
+		popupSubscribe.style.display ="none";
+	}
